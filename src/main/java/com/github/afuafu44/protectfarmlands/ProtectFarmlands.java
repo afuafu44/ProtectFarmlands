8bf1f4caf716f8dd774adfc26d3ae47846f92e86
afuafu <afuafu44@yahoo.co.jp>
2016-04-03 15:26:16 +0900

    first commit

---
+++ src/main/java/com/github/afuafu44/protectfarmlands/ProtectFarmlands.java
@@ -0,0 +1,24 @@
+package com.github.afuafu44.protectfarmlands;
+
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ProtectFarmlands extends JavaPlugin implements Listener {
+
+	@Override
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+
+	@EventHandler
+	public void onPlayerInteract(PlayerInteractEvent event) {
+		if ((event.getAction() == Action.PHYSICAL) && (event.getClickedBlock().getType() == Material.SOIL)) {
+			event.setCancelled(true);
+		}
+	}
+
+}
